09ce8dff59e622f06dcaade557f1582f355de8b8
rucm <rucm.info@gmail.com>
2018-09-09 17:07:16 +0900

    refactor

--- src/gameoflife.py
+++ src/gameoflife.py
@@ -1,37 +1,31 @@
 import os
 import sys
 import glob
+from kivy.logger import Logger
 from kivy.app import App
 from kivy.uix.boxlayout import BoxLayout
 from kivy.uix.button import Button
 from kivy.lang import Builder
 from kivy.uix.modalview import ModalView
 from kivy.properties import StringProperty
+from kivy.resources import resource_add_path
 
 
-def resourcePath():
+def resource_path():
     if hasattr(sys, '_MEIPASS'):
         return os.path.join(sys._MEIPASS)
-    cur_path = os.path.dirname(os.path.abspath(__file__))
-    return os.path.join(cur_path)
+    return os.path.join(os.path.dirname(os.path.abspath(__file__)))
 
 
-def loadStyle(dir_path):
+def load_style(dir_path):
     cur_path = os.path.dirname(os.path.abspath(__file__))
     style_files = glob.glob('{}/{}'.format(cur_path, dir_path))
     for style in style_files:
         Builder.load_file(style)
 
 
-class SampleModal(ModalView):
-    pass
-
-
 class GameOfLife(BoxLayout):
-
-    def modal(self):
-        sample_modal = SampleModal()
-        sample_modal.open()
+    pass
 
 
 class GameOfLifeApp(App):
@@ -41,7 +35,7 @@ class GameOfLifeApp(App):
 
 
 if __name__ == '__main__':
-    import kivy.resources
-    kivy.resources.resource_add_path(resourcePath())
-    loadStyle('layouts/*')
+    resource_add_path(resource_path())
+    load_style('layouts/*')
+
     GameOfLifeApp().run()
