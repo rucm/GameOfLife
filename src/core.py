76822c75b86aeb8949e1ab43b973d611ac6974dd
rucm <rucm.info@gmail.com>
2018-09-17 22:33:54 +0900

    Added bit mask  class.

--- src/core.py
+++ src/core.py
@@ -2,12 +2,25 @@ from bitarray import bitarray
 from random import randrange
 
 
+class mybitarray(bitarray):
+
+    def __lshift__(self, count):
+        return self[count:] + type(self)('0') * count
+
+    def __rshift__(self, count):
+        return type(self)('0') * count + self[:-count]
+
+    def __repr__(self):
+        return "{}('{}')".format(type(self).__name__, self.to01())
+
+
 class GameOfLifeCore(object):
 
     def __init__(self, cols, rows):
+        self.mask = GameOfLifeMask(cols, rows)
         self.cols = cols
         self.rows = rows
-        self.cells = bitarray(cols * rows)
+        self.cells = mybitarray(cols * rows)
         self.cells.setall(False)
 
     def __getitem__(self, coords):
@@ -22,9 +35,67 @@ class GameOfLifeCore(object):
         self.cells.setall(False)
 
     def next_step(self):
-        pass
+        # self.cells = self.mask.left(self.cells)
+        self.cells = self.mask.up_left(self.cells)
 
     def randomize(self):
         length = self.cells.length()
         for i in range(length):
             self.cells[i] = randrange(100) > 80
+
+
+class GameOfLifeMask:
+
+    def __init__(self, cols, rows):
+        self.cols = cols
+        self.rows = rows
+
+        self.left_mask = self._create_left()
+        self.up_left_mask = self._create_up_left()
+
+    def left(self, cells):
+        return (cells << 1) & self.left_mask
+
+    def up_left(self, cells):
+        return (cells << (self.cols + 1)) & self.up_left_mask
+
+    def down_left(self, cells):
+        pass
+
+    def right(self, cells):
+        pass
+
+    def up_right(self, cells):
+        pass
+
+    def down_right(self, cells):
+        pass
+
+    def up(self, cells):
+        pass
+
+    def down(self, cells):
+        pass
+
+    def _create_left(self):
+        mask = mybitarray(self.cols * self.rows)
+        mask.setall(True)
+        for i in range(self.rows):
+            mask[(i + 1) * self.cols - 1] = False
+        return mask
+    
+    def _create_up_left(self):
+        mask = mybitarray(self.cols * self.rows)
+        mask.setall(True)
+        for i in range(self.rows):
+            mask[(i + 1) * self.cols - 1] = False
+        for i in range(self.cols):
+            mask[-i] = False
+        return mask
+    
+    def _create_down_left(self):
+        mask = mybitarray(self.cols * self.rows)
+        mask.setall(True)
+        for i in range(self.rows):
+            mask[(i + 1) * self.cols - 1] = False
+        return mask
